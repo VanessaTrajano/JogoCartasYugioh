9c16f3bd82d73abf083171ccd2c214831bccb0cb
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2024-01-03 15:32:06 -0300

    Reorganizando código e implementando a função que remove as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,14 +13,15 @@ const state = {
     player: document.getElementById("player-field-card"),
     computer: document.getElementById("computer-field-card"),
   },
+  playerSides: {
+    player1: "player-cards",
+    player1Box: document.querySelector("#player-cards"),
+    computer: "computer-cards",
+    computerBox: document.querySelector("#computer-cards"),
+  },
   actions: {
     button: document.getElementById("next-duel"),
   },
-};
-
-const playerSides = {
-  player1: "player-cards",
-  computer: "computer-cards",
 };
 
 const pathImages = "./src/assets/icons/";
@@ -64,7 +65,7 @@ async function createCardImage(idCard, fieldSide) {
   cardImage.setAttribute("data-id", idCard);
   cardImage.classList.add("card");
 
-  if (fieldSide === playerSides.player1) {
+  if (fieldSide === state.playerSides.player1) {
     cardImage.addEventListener("mouseover", () => {
       drawSelectCard(idCard);
     });
@@ -94,6 +95,16 @@ async function setCardsField(cardId) {
   await drawButton(duelResult);
 }
 
+async function removeAllCardsImage() {
+  let {player1Box, computerBox} = state.playerSides;
+  
+  let imgElements = computerBox.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
+
+  imgElements = player1Box.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
+}
+
 async function drawSelectCard(index) {
   state.cardSprites.avatar.src = cardData[index].img;
   state.cardSprites.name.innerText = cardData[index].name;
@@ -109,8 +120,8 @@ async function drawCards(cardNumbers, fieldSide) {
 }
 
 function init() {
-  drawCards(5, playerSides.player1);
-  drawCards(5, playerSides.computer);
+  drawCards(5, state.playerSides.player1);
+  drawCards(5, state.playerSides.computer);
 }
 
 init();
